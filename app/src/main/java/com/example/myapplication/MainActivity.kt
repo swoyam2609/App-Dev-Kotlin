e544995c0480edd8957f87296a9c8bb4be210cd0
Swoyam Siddharth Nayak <96427746+swoyam2609@users.noreply.github.com>
2022-09-30 16:22:39 +0530

    Commit at 4:22 pm
    Created a Counter application which counts the number of times a button was clicked

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonClickMe = findViewById<Button>(R.id.myButton)
+//        buttonClickMe.text = "Haha"
+        var count=0
+        buttonClickMe.setOnClickListener {
+//            buttonClickMe.text = "Hey! You clicked me"
+            val textName=findViewById<TextView>(R.id.nameText)
+            count++
+            textName.text="Hey Swoyam\nYou Clicked me $count number of times"
+            Toast.makeText(this, "Hey, Click no. $count", Toast.LENGTH_SHORT).show()
+        }
     }
 }
