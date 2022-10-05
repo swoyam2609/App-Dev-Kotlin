570fb359bf1401b0e2934dc714e0b72660024e1a
Swoyam Siddharth Nayak <96427746+swoyam2609@users.noreply.github.com>
2022-10-05 18:48:06 +0530

    Added Button Functionality to Calculator App

--- Calculator/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var calculatorView : TextView? = null
+    private var lastNum = false
+    private var lastDot = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        calculatorView = findViewById(R.id.calculatorInput)
+    }
+
+    fun onButtonClick(view: View){
+        calculatorView?.append((view as Button).text)
+        lastNum = true
+    }
+
+    fun onClearClick(view: View){
+        calculatorView?.text=""
+        lastNum = false
+        lastDot = false
+    }
+
+    fun onDecimalClick(view: View){
+        if(lastNum && !lastDot){
+            calculatorView?.append((view as Button).text)
+            lastNum = false
+            lastDot = true
+        }
+        else{
+            Toast.makeText(this, "Invalid Operation", Toast.LENGTH_SHORT).show()
+        }
     }
 }
