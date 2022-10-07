c051bb0cd455e8b514f16e0eeb048ab7125afb8d
Swoyam Siddharth Nayak <96427746+swoyam2609@users.noreply.github.com>
2022-10-07 16:48:51 +0530

    Evening Commit

--- Calculator/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     private var calculatorView : TextView? = null
     private var lastNum = false
     private var lastDot = false
+    private var lastOperator = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,19 +23,42 @@ class MainActivity : AppCompatActivity() {
     fun onButtonClick(view: View){
         calculatorView?.append((view as Button).text)
         lastNum = true
+        lastOperator = false
     }
 
     fun onClearClick(view: View){
         calculatorView?.text=""
         lastNum = false
         lastDot = false
+        lastOperator = false
     }
 
     fun onDecimalClick(view: View){
         if(lastNum && !lastDot){
             calculatorView?.append((view as Button).text)
             lastNum = false
+            lastOperator = false
             lastDot = true
+        }
+        else{
+            Toast.makeText(this, "Invalid Operation", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun onOperatorClick(view: View){
+        if(calculatorView?.text?.contentEquals("") == true && (view as Button).text.contentEquals("-")){
+            calculatorView?.append((view as Button).text)
+            lastNum=false
+            lastDot=false
+            lastOperator=true
+        }
+        else if(!lastOperator && lastNum){
+            calculatorView?.append(" ")
+            calculatorView?.append((view as Button).text)
+            calculatorView?.append(" ")
+            lastNum=false
+            lastDot=false
+            lastOperator=true
         }
         else{
             Toast.makeText(this, "Invalid Operation", Toast.LENGTH_SHORT).show()
